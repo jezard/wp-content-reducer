197a4eb1be7d234e450c93387f8b11b19ebd934d
Jeremy Summers <jez@wizard.technology>
2022-06-30 20:09:14 +0100

    Descend into child directory

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	dirs, _ := os.ReadDir(".")
+	dirs, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 	for i, e := range dirs {
 		fmt.Println(i, e.Name())
+		if e.IsDir() { // TODO should be recursive
+			os.Chdir(e.Name())
+			dirs1, err := os.ReadDir(".")
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			for j, f := range dirs1 {
+				fmt.Println(strconv.Itoa(i)+"."+strconv.Itoa(j), "_"+f.Name())
+			}
+		}
 	}
 }
