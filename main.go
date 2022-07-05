79893a89362eaa6b47d4cbb33dc9f5cf399045cd
Jeremy Summers <jez@wizard.technology>
2022-07-05 20:16:11 +0100

    sort out path entry

--- main.go
+++ main.go
@@ -9,23 +9,34 @@ import (
 	"strconv"
 )
 
+// test usage go run . /Users/jeremy/Library/
 func main() {
+	const env = "dev"
+	var csvPath = ""
+
+	if env == "dev" { //write to a directory where we can read it
+		csvPath, _ = os.Getwd()
+	} else {
+		ex, _ := os.Executable()
+		csvPath = filepath.Dir(ex)
+	}
 
 	if len(os.Args) < 2 {
 		fmt.Println("Directory argument has not been provided")
 		return
 	}
 
-	cwd, _ := os.Getwd()
 	targetDir := os.Args[1]
 	maxDepth := flag.Int("depth", 0, "Maximum recursion levels")
 
-	err := os.Chdir(cwd + "/" + targetDir)
+	err := os.Chdir(targetDir)
 
 	if err != nil {
+		fmt.Println("Directory argument is not a directory: ", targetDir)
+	} else {
 		cwd, _ := os.Getwd()
 
-		f, err := os.Create("queue.csv")
+		f, err := os.Create(csvPath + "/queue.csv")
 		if err != nil {
 			fmt.Println("Error: Could not open queue file.")
 		}
@@ -43,8 +54,6 @@ func main() {
 		if err != nil {
 			fmt.Println("Couldn't write queue from buffer to file: ", err)
 		}
-	} else {
-		fmt.Println("Directory argument is not a directory")
 	}
 }
 
@@ -64,7 +73,7 @@ func recurse(dirName string, depth int, maxDepth int, w *bufio.Writer) {
 	for _, e := range dirEntries {
 		if !e.IsDir() {
 			//filepath,status,thread
-			fmt.Fprint(w, filepath.FromSlash(dirName+"/"+e.Name())+",0,0\r\n")
+			fmt.Fprint(w, filepath.FromSlash("\""+dirName+"/"+e.Name())+"\",0,0\r\n")
 			fmt.Println(getIndent(depth), e.Name())
 		}
 	}
